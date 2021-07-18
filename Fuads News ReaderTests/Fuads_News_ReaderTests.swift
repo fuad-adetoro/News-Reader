2de8c2b7263c442397ac6716167e340fc29562e7
Fuad Adetoro <fuad.adetoro96@gmail.com>
2021-07-18 11:24:37 +0100

    Initial Commit

---
+++ Fuads News ReaderTests/Fuads_News_ReaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fuads_News_ReaderTests.swift
+//  Fuads News ReaderTests
+//
+//  Created by FA on 18/07/2021.
+//
+
+import XCTest
+@testable import Fuads_News_Reader
+
+class Fuads_News_ReaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
